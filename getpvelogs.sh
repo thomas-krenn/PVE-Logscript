1ec04882fc159a50d45f6fbae13bc9bdc27db847
Samuel Müller <smueller@thomas-krenn.com>
2026-03-03 14:54:22 +0100

    Update version to 4.0.4 in getpvelogs.sh and README.md to reflect the latest release.

--- getpvelogs.sh
+++ getpvelogs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Version: 4.0.3 - 01/2026
+# Version: 4.0.4 - 03/2026
 # Thomas-Krenn.AG - Proxmox VE Support Log Collector
 # Author: Samuel Mueller
 # Contact: smueller@thomas-krenn.com
@@ -53,7 +53,7 @@ shopt -s nullglob
 shopt -s lastpipe
 
 # ---------- Constants ----------
-readonly VERSION="4.0.3"
+readonly VERSION="4.0.4"
 readonly MIN_DISK_SPACE_MB=500
 readonly CMD_TIMEOUT=60
 
